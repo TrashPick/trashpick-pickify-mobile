f4bcfc6b8ad15873bb5a3a7d5243a9e61931d223
unknown <kwameagyenimboateng64@gmail.com>
2020-02-10 10:27:19 +0000

    added new screens

---
+++ screens/Message/Message.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native'
+
+class Message extends React.Component{
+    render(){
+        return(
+                <View>
+                    <Text>Message</Text>
+                </View>
+        )
+    }
+}
+export default Message;
+
+const styles = StyleSheet.create({
+
+})
---
+++ screens/Settings/Settings.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native'
+
+class Settings extends React.Component{
+    render(){
+        return(
+                <View>
+                    <Text>Settings</Text>
+                </View>
+        )
+    }
+}
+export default Settings;
+
+const styles = StyleSheet.create({
+
+})
