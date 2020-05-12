6b7da01226ce10aa4b2b760895b71ae212763b31
unknown <kwameagyenimboateng64@gmail.com>
2020-05-12 00:39:33 +0000

    added colors

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,7 +1,24 @@
 const tintColor = '#2f95dc';
+const green = '#5DDC92';
+const black = '#333333';
+const textHeader = '#4F4F4F';
+const textColor = '#828282';
+const border = '#F2F2F2';
+const white = '#FCFCFC';
+const grey = '#A6AAB4';
+const textBackground = '#F0FBF3';
+
 
 export default {
   tintColor,
+  green,
+  black,
+  textHeader,
+  textColor,
+  border,
+  white,
+  grey,
+  textBackground,
   tabIconDefault: '#ccc',
   tabIconSelected: tintColor,
   tabBar: '#fefefe',
