cc5912aea8e94bc1cb9785d4237a8e73a3f326af
unknown <kwameagyenimboateng64@gmail.com>
2020-05-12 04:28:41 +0000

    add forgotpass screen

---
+++ screens/Auth/Forgot.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View,StyleSheet } from 'react-native'
+import Colors from '../../constants/Colors'
+
+
+export default function ForgotPassword(){
+    return(
+        <View style={styles.container}>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1
+    }
+})
