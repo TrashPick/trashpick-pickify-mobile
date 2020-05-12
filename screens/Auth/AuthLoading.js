cbfd1894f8969afb8cc262fbe9c699f02bf2347d
unknown <kwameagyenimboateng64@gmail.com>
2020-05-12 02:58:40 +0000

    add authloading

---
+++ screens/Auth/AuthLoading.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+	View,
+	ActivityIndicator,
+	StyleSheet,
+	StatusBar,
+	AsyncStorage
+} from 'react-native';
+
+export default class AuthLoading extends React.Component {
+	constructor(props) {
+		super(props);
+		this.bootstrapAsync();
+	}
+	bootstrapAsync = async () => {
+		const usertoken = await AsyncStorage.getItem('userToken');
+		console.log(usertoken);
+		if (usertoken === null) {
+			this.props.navigation.navigate('Auth');
+		} else {
+			this.props.navigation.navigate('Main');
+		}
+	};
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<StatusBar barStyle="dark-content" backgroundColor="white" />
+				<ActivityIndicator color="green" />
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center'
+	}
+});
