40405f400af6e00e5a2369b78bd0e0e2b7e6e089
unknown <kwameagyenimboateng64@gmail.com>
2020-05-13 15:13:11 +0000

    created verification screen

--- screens/Auth/Verification.js
+++ screens/Auth/Verification.js
@@ -1,18 +1,76 @@
 import React from 'react'
-import { View,StyleSheet } from 'react-native'
+import { View,StyleSheet,Text,TouchableNativeFeedback,SafeAreaView,KeyboardAvoidingView,Plaform,TouchableOpacity } from 'react-native'
 import Colors from '../../constants/Colors'
+import Colors from '../../constants/Colors'
+import Fonts from '../../constants/Fonts'
+import Button from '../../components/Button'
+import Header from '../../components/Header'
+import TextInput from '../../components/TextInput'
+import {AntDesign} from '@expo/vector-icons'
 
 
 export default function Verification(){
+    const renderBackBtn = ()=> {
+        return (
+          <View>
+            {Platform.OS === 'ios' ?
+            <TouchableOpacity activeOpacity={0.8} onPress={() =>navigation.pop()}>
+            <View style={styles.backBtn}>
+              <AntDesign name="arrowleft" color="black" size={18} />
+            </View>
+          </TouchableOpacity>
+          :
+          <TouchableNativeFeedback onPress={() =>navigation.pop()} background={TouchableNativeFeedback.SelectableBackgroundBorderless()} useForeground={true} >
+            <View style={styles.backBtn}>
+              <AntDesign name="arrowleft" color="black" size={18} />
+            </View>
+          </TouchableNativeFeedback>
+            }
+          </View>
+        );
+  }
     return(
-        <View style={styles.container}>
+        <SafeAreaView style={{flex:1}}>
+            <View style={styles.container}>
+            <Header leftAction={renderBackBtn()} title="Enter phoneNumber" />
+            <KeyboardAvoidingView
+						style={{ flex: 1, justifyContent: "center" }}
+						behavior={Platform.OS === "android" ? "height" : "padding"}
+						keyboardVerticalOffset={10}
+						>
 
-        </View>
+                <View style={styles.btnContainer}>
+                     <Button type="signup" onClick={()=>{navigation.navigate("Otp")}} title="Continue"/>
+                </View>
+            </KeyboardAvoidingView>
+            </View>
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container:{
         flex:1
-    }
+    },
+    backBtn:{
+        padding:15
+    },
+    emailContainer:{
+      paddingVertical:20
+    },
+    passwordContainer:{
+    },
+    btnContainer:{
+      flex:1,
+      justifyContent:'flex-end',
+      paddingHorizontal:20,
+      marginBottom:40
+  },
+  more:{
+    fontFamily:"Medium",
+    fontSize:Fonts.t2,
+    textAlign:'center',
+    letterSpacing:-0.2,
+    color:Colors.grey,
+  }
 })
