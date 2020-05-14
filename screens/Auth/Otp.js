f4b7613368dff50300d894aa81cda8b0264b8fa8
unknown <kwameagyenimboateng64@gmail.com>
2020-05-14 22:36:44 +0000

    complete otp screen

--- screens/Auth/Otp.js
+++ screens/Auth/Otp.js
@@ -1,19 +1,19 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { View,StyleSheet,Text,TouchableNativeFeedback,SafeAreaView,KeyboardAvoidingView,Plaform,TouchableOpacity } from 'react-native'
-import Colors from '../../constants/Colors'
 import Colors from '../../constants/Colors'
 import Fonts from '../../constants/Fonts'
 import Button from '../../components/Button'
 import Header from '../../components/Header'
 import TextInput from '../../components/TextInput'
 import {AntDesign} from '@expo/vector-icons'
+import {CodeField,Cursor,useBlurOnFulfill,useClearByFocusCell} from 'react-native-confirmation-code-field';
 
 
-export default function Otp(){
-    const [code,setCode] = useState("");
-    const codeChange = (target)=>{
-        setCode(target.nativeEvent.text)
-        }
+export default function Otp({navigation}){
+  const CELL_COUNT = 5;
+  const [value, setValue] = useState('');
+  const ref = useBlurOnFulfill({value, cellCount: CELL_COUNT});
+  const [props, getCellOnLayoutHandler] = useClearByFocusCell({ value,setValue,});
     const renderBackBtn = ()=> {
         return (
           <View>
@@ -36,17 +36,33 @@ export default function Otp(){
     return(
         <SafeAreaView style={{flex:1}}>
             <View style={styles.container}>
-            <Header leftAction={renderBackBtn()} title="Verify Code" />
+            <Header leftAction={renderBackBtn()} title="Confirmation Code" />
             <KeyboardAvoidingView
 						style={{ flex: 1, justifyContent: "center" }}
 						behavior={Platform.OS === "android" ? "height" : "padding"}
 						keyboardVerticalOffset={10}
 						>
-                <View style={{paddingVertical:10,justifyContent:'center',alignItems:'center',paddingHorizontal:50}}>
-                      <Text style={{fontFamily:'Medium',fontSize:16,color:Colors.grey,textAlign:'center',lineHeight:25}}>Enter the code sent to (0500486323)</Text>
+                <View style={{paddingVertical:10,justifyContent:'center',alignItems:'center',paddingHorizontal:50,flex:1}}>
+                      <Text style={{fontFamily:'Medium',fontSize:16,color:Colors.grey,textAlign:'center',lineHeight:25}}>Enter the 5- digits code sent to (0500486323)</Text>
                   </View>
-              <View style={styles.codeContainer}>
-                <TextInput input="number" placeholder="Enter here" onChange={value =>{codeChange(value)}}/>
+                  <View style={{paddingHorizontal:30,justifyContent:'center',flex:1}}>
+                <CodeField
+                    ref={ref}
+                    {...props}
+                    value={value}
+                    onChangeText={setValue}
+                    cellCount={CELL_COUNT}
+                    rootStyle={styles.codeFiledRoot}
+                    keyboardType="number-pad"
+                    renderCell={({index, symbol, isFocused}) => (
+                      <Text
+                        key={index}
+                        style={[styles.cell, isFocused && styles.focusCell]}
+                        onLayout={getCellOnLayoutHandler(index)}>
+                        {symbol || (isFocused ? <Cursor /> : null)}
+                      </Text>
+                    )}
+                  />
               </View>
                 <View style={styles.btnContainer}>
                      <Button type="signup" onClick={()=>{navigation.navigate("Signup")}} title="Verify"/>
@@ -64,9 +80,6 @@ const styles = StyleSheet.create({
     backBtn:{
         padding:15
     },
-    codeContainer:{
-      paddingVertical:20
-    },
     btnContainer:{
       flex:1,
       justifyContent:'flex-end',
@@ -79,5 +92,30 @@ const styles = StyleSheet.create({
     textAlign:'center',
     letterSpacing:-0.2,
     color:Colors.grey,
-  }
+  },
+  root: {
+    flex: 1,
+    padding: 20
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 30,
+  },
+  codeFiledRoot: {
+    marginTop: 20
+  },
+  cell: {
+    width: 40,
+    height: 40,
+    lineHeight: 38,
+    fontSize: 24,
+    borderBottomWidth: 2,
+    borderBottomColor:Colors.otpColor,
+    textAlign: 'center',
+    fontFamily:"Medium",
+    color:Colors.black
+  },
+  focusCell: {
+    borderBottomColor:Colors.green,
+  },
 })
