a36ed7515c43a98c235764841ddb9d73dce114fe
unknown <kwameagyenimboateng64@gmail.com>
2020-05-19 11:48:18 +0000

    add props to icons

--- components/TabBarIcon.js
+++ components/TabBarIcon.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { Ionicons } from '@expo/vector-icons';
-
+import { Ionicons,MaterialIcons,SimpleLineIcons,Octicons } from '@expo/vector-icons';
 import Colors from '../constants/Colors';
 
 export default function TabBarIcon(props) {
+ if(props.icon === 'home'){
   return (
-    <Ionicons
+    <Octicons
       name={props.name}
       size={26}
       style={{ marginBottom: -3 }}
-      color={props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+      color={props.focused ? Colors.green : Colors.tabIconDefault}
     />
   );
+ }else if(props.icon ==='notification'){
+  return (
+    <MaterialIcons
+      name={props.name}
+      size={26}
+      style={{ marginBottom: -3 }}
+      color={props.focused ? Colors.green : Colors.tabIconDefault}
+    />
+  );
+ }
 }
