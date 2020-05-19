552bb4b4533821d3700958130800b23c1722e7d2
unknown <kwameagyenimboateng64@gmail.com>
2020-05-19 11:48:34 +0000

    modified request icon

--- components/Button.js
+++ components/Button.js
@@ -3,6 +3,7 @@ import { View,StyleSheet,Text,TouchableWithoutFeedback,Animated,Platform,Touchab
 import Font from '../constants/Fonts'
 import Colors from '../constants/Colors'
 import Signin from '../screens/Auth/Signin'
+import { Ionicons } from '@expo/vector-icons';
 export default class  Button extends React.Component{
   constructor(props){
     super(props)
@@ -63,6 +64,24 @@ export default class  Button extends React.Component{
            }
          </View>
       )
+       }else if(this.props.type === "request"){
+        return(
+         <View>
+           {Platform.OS === 'ios' ?
+           <TouchableOpacity activeOpacity={1} onPress={this.props.onClick}  onPressIn={()=>{this.handlePressIn()}} onPressOut={()=>{this.handlePressOut()}}>
+                  <Animated.View  style={[styles.request,animationStyle]}>
+                      <Ionicons name="ios-add" size={24} color="black" />
+                  </Animated.View>
+          </TouchableOpacity>
+          :
+          <TouchableWithoutFeedback onPress={this.props.onClick}  onPressIn={()=>{this.handlePressIn()}} onPressOut={()=>{this.handlePressOut()}}>
+                  <Animated.View  style={[styles.request,animationStyle]}>
+                       <Ionicons name="ios-add" size={24} color={this.props.focused ? "white" : "white"} />
+                  </Animated.View>
+          </TouchableWithoutFeedback>
+           }
+         </View>
+      )
        }
     }
 }
@@ -87,6 +106,30 @@ const styles = StyleSheet.create({
       borderRadius:5,
       padding:18,
       bottom:0
+  },
+  request:{
+        alignItems:'center',
+        justifyContent:'center',
+        backgroundColor:Colors.green,
+        width:62,
+        height:62,
+        borderRadius: 36,
+        borderWidth:3,
+        borderColor:"white",
+        position:'absolute',
+        top:-50,
+        left:-30,
+        ...Platform.select({
+            ios: {
+              shadowColor: 'black',
+              shadowOffset: { width: 0, height: 10 },
+              shadowOpacity: 0.3,
+              shadowRadius: 5,
+            },
+            android: {
+              elevation: 3,
+            },           
+}),
   },
   text2:{
       fontFamily:"Medium",
