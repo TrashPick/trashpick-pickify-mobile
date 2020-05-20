83da45a582efeac626aec868b89dc6b1560da89d
unknown <kwameagyenimboateng64@gmail.com>
2020-05-20 01:52:48 +0000

    add request icon

--- components/TabBarIcon.js
+++ components/TabBarIcon.js
@@ -22,4 +22,14 @@ export default function TabBarIcon(props) {
     />
   );
  }
+ else if(props.icon ==='request'){
+  return (
+    <Ionicons
+      name={props.name}
+      size={26}
+      style={{ marginBottom: -3 }}
+      color={props.focused ? Colors.green : Colors.tabIconDefault}
+    />
+  );
+ }
 }
