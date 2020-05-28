9de1e911bd989d33b9f28b69863a114830b465d6
unknown <kwameagyenimboateng64@gmail.com>
2020-05-28 00:18:53 +0000

    added avatar and name

--- navigation/Drawer.js
+++ navigation/Drawer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { DrawerItems,createDrawerNavigator } from 'react-navigation-drawer';
 import { createStackNavigator } from 'react-navigation-stack';
-import { SafeAreaView,StyleSheet,View,ScrollView,Text } from 'react-native'
+import { SafeAreaView,StyleSheet,View,ScrollView,Text, Platform } from 'react-native'
 import { EvilIcons,Ionicons,SimpleLineIcons,AntDesign,Feather } from '@expo/vector-icons';
 import Search from '../screens/Search/Search';
 import Help from '../screens/Help/Help';
@@ -10,6 +10,8 @@ import Profile from '../screens/Profile/Profile';
 import MainTabNavigator from './MainTabNavigator';
 import Colors from '../constants/Colors'
 import Layout from '../constants/Layout'
+import Fonts from '../constants/Fonts'
+import UserAvatar from 'react-native-user-avatar'
 
 const Label = (props)=>(
  <View style={styles.labelContainer}>
@@ -18,8 +20,18 @@ const Label = (props)=>(
 )
 const CustomComponent = (props)=>(
   <SafeAreaView style={{flex:1}}>
-      <View style={{justifyContent:'center',alignItems:'center',paddingVertical:20}}>
-          <EvilIcons name="user" size={104} color={Colors.grey} />
+      <View style={{flexDirection:'row',justifyContent:'center',alignItems:'center',paddingVertical:30,}}>
+          {/* <EvilIcons name="user" size={104} color={Colors.grey} /> */}
+          <UserAvatar size={50} name="Kwame Agyenim" bgColor={Colors.green} />
+          {Platform === "ios" ? 
+          <View style={{flexDirection:'column',}}>
+            <Text style={styles.title}>Kwame Agyenim - Boateng</Text>
+            <Text style={styles.text}>kwameagyenimboateng64@gmail.com</Text>   
+          </View>:
+          <View style={{flexDirection:'column',paddingLeft:20}}>
+            <Text style={styles.title}>Kwame Agyenim - Boateng</Text>
+            <Text style={styles.text}>kwameagyenimboateng64@gmail.com</Text>   
+          </View>}
       </View>
     <ScrollView showsVerticalScrollIndicator={false}>
         <DrawerItems {...props} />
@@ -64,7 +76,6 @@ const MainDrawer = createDrawerNavigator(
    ProfileStack:{
      screen:ProfileStack,
      navigationOptions:{
-      //  drawerLockMode:'locked-closed',
        drawerIcon:({focused})=>(
         // <AntDesign focused={focused} name="user" size={22} color={Colors.black}/>
         <Feather focused={focused} name="user" size={24}  color={Colors.black} />
@@ -77,7 +88,6 @@ const MainDrawer = createDrawerNavigator(
    HelpStack:{
      screen:HelpStack,
      navigationOptions:{
-      // drawerLockMode:'locked-closed',
       drawerIcon:({focused})=>(
         // <Ionicons focused={focused} name="ios-help-circle-outline" size={22} color={Colors.black} />
         <Feather focused={focused} name="alert-circle" size={22} color={Colors.black}   />
@@ -103,8 +113,8 @@ const MainDrawer = createDrawerNavigator(
 
 },{
   contentComponent:CustomComponent,
-  drawerType:"slide",
-  // drawerWidth:300,
+  // drawerType:"slide",
+  drawerWidth:350,
   contentOptions: {
         activeTintColor: '#e91e63',
         inactiveTintColor:Colors.grey,
@@ -129,5 +139,17 @@ const styles = StyleSheet.create({
     fontFamily:'Medium',
     fontSize:16,
     color:Colors.black
+  },
+  title:{
+    fontFamily:"Medium",
+    fontSize:Fonts.t2,
+    letterSpacing:-0.2,
+    color:Colors.black, 
+  },
+  text:{
+    fontFamily:"Medium",
+    fontSize:14,
+    letterSpacing:-0.2,
+    color:Colors.grey,
   }
 });
