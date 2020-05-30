9213292fe244f85385f7abf5b0099e4acfa037c4
unknown <kwameagyenimboateng64@gmail.com>
2020-05-30 22:30:26 +0000

    add new pickups

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -18,8 +18,10 @@ function Home({navigation}){
         {id:6,date:"Sat, 6 Feb",company:"Zoom Lion Int Com",category:"Plastics"},
         {id:7,date:"Sun, 4 Feb",company:"Super paper product co.Ltd",category:"Plastics"},
         {id:8,date:"Fri, 4 Jan",company:"Geocrest co.Ltd",category:"Plastics"},
-        {id:9,date:"Fri, 2 Jan",company:"Fearless co.Ltd",category:"Plastics"},
-        {id:10,date:"Fri, 1 Jan",company:"Glocost co.Ltd",category:"Plastics"},
+        {id:9,date:"Wed, 2 Jan",company:"Fearless co.Ltd",category:"Plastics"},
+        {id:10,date:"Tues, 27 Dec",company:"Glocost co.Ltd",category:"Plastics"},
+        {id:11,date:"Tues, 17 Dec",company:"Django co.Ltd",category:"Plastics"},
+        {id:12,date:"Fri, 1 Nov",company:"RollerCoaster co.Ltd",category:"Plastics"},
     ]
     const ListItem = (props)=>{
         return(
