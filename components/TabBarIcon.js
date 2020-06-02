3104820fba148b8d676ddfca9bcb7d961af9a882
unknown <kwameagyenimboateng64@gmail.com>
2020-06-02 10:58:41 +0000

    changed icon

--- components/TabBarIcon.js
+++ components/TabBarIcon.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Ionicons,MaterialIcons,SimpleLineIcons,Octicons } from '@expo/vector-icons';
+import { Ionicons,MaterialCommunityIcons,Feather,Octicons } from '@expo/vector-icons';
 import Colors from '../constants/Colors';
 
 export default function TabBarIcon(props) {
  if(props.icon === 'home'){
   return (
-    <Octicons
+    <MaterialCommunityIcons
       name={props.name}
       size={26}
       style={{ marginBottom: -3 }}
       color={props.focused ? Colors.green : Colors.tabIconDefault}
     />
   );
- }else if(props.icon ==='notification'){
+ }else if(props.icon ==='account'){
   return (
-    <MaterialIcons
+    <Feather
       name={props.name}
       size={26}
       style={{ marginBottom: -3 }}
