036fe39da4a21b8e145124859b2c4a8bc35258f1
Mushud <mushudkofiabuu@gmail.com>
2020-07-26 13:06:00 +0000

    Added new routing

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,40 +1,42 @@
-import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-import MainTabNavigator from './MainTabNavigator';
-import MainDrawer from './Drawer'
-import Details from '../screens/Auth/Details'
-import Welcome from '../screens/Auth/Welcome'
-import Signup from '../screens/Auth/Signup'
-import Signin from '../screens/Auth/Signin'
-import Forgot from '../screens/Auth/Forgot'
-import Verify from '../screens/Auth/Verification'
-import Otp from '../screens/Auth/Otp'
-import Splash from '../screens/Auth/Splash'
-import AuthLoading from '../screens/Auth/AuthLoading'
-import { createStackNavigator } from 'react-navigation-stack';
+import React from "react";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import MainTabNavigator from "./MainTabNavigator";
+import MainDrawer from "./Drawer";
+import Details from "../screens/Auth/Details";
+import Welcome from "../screens/Auth/Welcome";
+import Signup from "../screens/Auth/Signup";
+import Signin from "../screens/Auth/Signin";
+import Forgot from "../screens/Auth/Forgot";
+import Verify from "../screens/Auth/Verification";
+import Otp from "../screens/Auth/Otp";
+import Splash from "../screens/Auth/Splash";
+import AuthLoading from "../screens/Auth/AuthLoading";
+import { createStackNavigator } from "react-navigation-stack";
 
-const FirstAuthStack = createStackNavigator({
-  Details:Details,
-},
-{ initialRouteName:'Details',headerMode:"none"}
-)
-const SecondAuthStack = createStackNavigator({
-  Welcome:Welcome,
-  Signup:Signup,
-  Signin:Signin,
-  Forgot:Forgot,
-  Verify:Verify,
-  Otp:Otp,
-  Splash:Splash,
-},
-  { headerMode:"none"}
-)
+const FirstAuthStack = createStackNavigator(
+  {
+    Details: Details,
+  },
+  { initialRouteName: "Details", headerMode: "none" }
+);
+const SecondAuthStack = createStackNavigator(
+  {
+    Welcome: Welcome,
+    Signup: Signup,
+    Signin: Signin,
+    Forgot: Forgot,
+    Verify: Verify,
+    Otp: Otp,
+    Splash: Splash,
+  },
+  { headerMode: "none" }
+);
 export default createAppContainer(
   createSwitchNavigator({
     // You could add another route here for authentication.
-    AuthLoading:AuthLoading,
-    AuthDetails:FirstAuthStack,
-    Auth:SecondAuthStack,
+    AuthLoading: AuthLoading,
+    // AuthDetails:FirstAuthStack,
+    Auth: SecondAuthStack,
     Main: MainDrawer,
   })
 );
