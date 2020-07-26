8be8eebe2a6b99f6bb7bd370d550b9658b4acd17
Mushud <mushudkofiabuu@gmail.com>
2020-07-26 13:06:14 +0000

    Fixed registration of user

--- screens/Auth/AuthLoading.js
+++ screens/Auth/AuthLoading.js
@@ -1,41 +1,41 @@
-import React from 'react';
+import React from "react";
 import {
-	View,
-	ActivityIndicator,
-	StyleSheet,
-	StatusBar,
-	AsyncStorage
-} from 'react-native';
+  View,
+  ActivityIndicator,
+  StyleSheet,
+  StatusBar,
+  AsyncStorage,
+} from "react-native";
 
 export default class AuthLoading extends React.Component {
-	constructor(props) {
-		super(props);
-		this.bootstrapAsync();
-	}
-	bootstrapAsync = async () => {
-		const usertoken = await AsyncStorage.getItem('userToken');
-		console.log(usertoken);
-		if (usertoken === null) {
-			this.props.navigation.navigate('AuthDetails');
-		} else {
-			this.props.navigation.navigate('Main');
-		}
-	};
+  constructor(props) {
+    super(props);
+    this.bootstrapAsync();
+  }
+  bootstrapAsync = async () => {
+    const usertoken = await AsyncStorage.getItem("userToken");
+    console.log(usertoken);
+    if (usertoken === null) {
+      this.props.navigation.navigate("Auth");
+    } else {
+      this.props.navigation.navigate("Main");
+    }
+  };
 
-	render() {
-		return (
-			<View style={styles.container}>
-				<StatusBar barStyle="dark-content" backgroundColor="white" />
-				<ActivityIndicator color="green" />
-			</View>
-		);
-	}
+  render() {
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="dark-content" backgroundColor="white" />
+        <ActivityIndicator color="green" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center'
-	}
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
