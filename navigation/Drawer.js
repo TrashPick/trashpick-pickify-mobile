68cc9a549764033ec8c28045a97c03e7b57ae1c8
unknown <kwameagyenimboateng64@gmail.com>
2020-10-08 14:30:01 +0000

    modify drawer style

--- navigation/Drawer.js
+++ navigation/Drawer.js
@@ -19,10 +19,9 @@ const Label = (props)=>(
  </View>
 )
 const CustomComponent = (props)=>(
-  <SafeAreaView style={{flex:1}}>
-      <View style={{flexDirection:'row',justifyContent:'center',alignItems:'center',paddingVertical:50,}}>
-          {/* <EvilIcons name="user" size={104} color={Colors.grey} /> */}
-          <UserAvatar size={50} name="Kwame Agyenim" bgColor={Colors.green} />
+  <SafeAreaView style={{flex:1,}}>
+      <View style={{flex:1,flexDirection:'row',justifyContent:'center',alignItems:'center',paddingVertical:0,paddingHorizontal:40,}}>
+          <UserAvatar size={60} name="Kwame Agyenim" bgColor={Colors.green} />
           {Platform === "ios" ? 
           <View style={{flexDirection:'column',}}>
             <Text style={styles.title}>Kwame Agyenim - Boateng</Text>
@@ -30,7 +29,7 @@ const CustomComponent = (props)=>(
           </View>:
           <View style={{flexDirection:'column',paddingLeft:10}}>
             <Text style={styles.title}>Kwame Agyenim - Boateng</Text>
-            <Text style={styles.text}>kwameagyenimboateng64@gmail.com</Text>   
+            <Text style={styles.text} ellipsizeMode="tail" numberOfLines={1}>kwameagyenimboateng64@gmail.comsasdsadasd</Text>   
           </View>}
           <View style={{borderBottomColor:Colors.border,borderBottomWidth:1}}></View>
       </View>
@@ -114,12 +113,13 @@ const MainDrawer = createDrawerNavigator(
 },{
   contentComponent:CustomComponent,
   // drawerType:"slide",
-  drawerWidth:350,
+  drawerWidth:330,
   contentOptions: {
         activeTintColor:Colors.green,
         inactiveTintColor:Colors.grey,
         itemsContainerStyle: {
           marginVertical: 0,
+          // marginHorizontal:20
         },
         
       }
@@ -132,7 +132,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   labelContainer:{
-    paddingVertical:15
+    paddingVertical:25
   },
   label:{
     textAlign:'center',
@@ -143,13 +143,13 @@ const styles = StyleSheet.create({
   title:{
     fontFamily:"Medium",
     fontSize:Fonts.t2,
-    letterSpacing:-0.2,
+    letterSpacing:0.2,
     color:Colors.black, 
   },
   text:{
     fontFamily:"Medium",
     fontSize:14,
-    letterSpacing:-0.2,
+    letterSpacing:0.2,
     color:Colors.textColor,
   }
 });
