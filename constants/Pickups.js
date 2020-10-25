1213a5f516a9bbcb278bd7a6167f8a5b4497ad90
unknown <kwameagyenimboateng64@gmail.com>
2020-10-25 17:23:57 +0000

    create pickups file

---
+++ constants/Pickups.js
@@ -0,0 +1,14 @@
+export const data = [
+    {id:1,date:"Wed, 25 May",company:"Coliba Ghana",category:"Plastics"},
+    {id:2,date:"Mon, 14 April",company:"Twelve Baskets Service",category:"Plastics"},
+    {id:3,date:"Fri, 4 April",company:"GP waste Ltd",category:"Plastics"},
+    {id:4,date:"Thurs, 29 March",company:"Polytex Ind.Ltd",category:"Plastics"},
+    {id:5,date:"Mon, 20 March",company:"3G plastic limited company",category:"Plastics"},
+    {id:6,date:"Sat, 6 Feb",company:"Zoom Lion Int Com",category:"Plastics"},
+    {id:7,date:"Sun, 4 Feb",company:"Super paper product co.Ltd",category:"Plastics"},
+    {id:8,date:"Fri, 4 Jan",company:"Geocrest co.Ltd",category:"Plastics"},
+    {id:9,date:"Wed, 2 Jan",company:"Fearless co.Ltd",category:"Plastics"},
+    {id:10,date:"Tues, 27 Dec",company:"Glocost co.Ltd",category:"Plastics"},
+    {id:11,date:"Tues, 17 Dec",company:"Django co.Ltd",category:"Plastics"},
+    {id:12,date:"Fri, 1 Nov",company:"RollerCoaster co.Ltd",category:"Plastics"},
+]
